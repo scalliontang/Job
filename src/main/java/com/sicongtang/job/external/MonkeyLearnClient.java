7f9215a67ac482d12673e1d37d8b3056def1a470
Sicong Tang <scalliontang@gmail.com>
2021-03-15 10:55:42 -0400

    Minor changes

---
+++ src/main/java/com/sicongtang/job/external/MonkeyLearnClient.java
@@ -0,0 +1,86 @@
+package com.sicongtang.job.external;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sicongtang.job.entity.ExtractRequestBody;
+import com.sicongtang.job.entity.ExtractResponseItem;
+import com.sicongtang.job.entity.Extraction;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.*;
+
+public class MonkeyLearnClient {
+
+    private static final String EXTRACT_URL = "https://api.monkeylearn.com/v3/extractors/ex_YCya9nrn/extract/";
+    private static final String AUTH_TOKEN = "6898d2ef87eba4a383309e65443edf54d623a08c";
+
+    public List<Set<String>> extract(List<String> articles) {
+        ObjectMapper mapper = new ObjectMapper();
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        HttpPost request = new HttpPost(EXTRACT_URL);
+        request.setHeader("Content-type", "application/json");
+        request.setHeader("Authorization", "Token " + AUTH_TOKEN);
+        ExtractRequestBody body = new ExtractRequestBody(articles, 3);
+
+        String jsonBody;
+        try {
+            jsonBody = mapper.writeValueAsString(body);
+        } catch (JsonProcessingException e) {
+            return Collections.emptyList();
+        }
+
+        try {
+            request.setEntity(new StringEntity(jsonBody));
+        } catch (UnsupportedEncodingException e) {
+            return Collections.emptyList();
+        }
+
+        ResponseHandler<List<Set<String>>> responseHandler = response -> {
+            if (response.getStatusLine().getStatusCode() != 200) {
+                return Collections.emptyList();
+            }
+            HttpEntity entity = response.getEntity();
+            if (entity == null) {
+                return Collections.emptyList();
+            }
+            ExtractResponseItem[] results = mapper.readValue(entity.getContent(), ExtractResponseItem[].class);
+            List<Set<String>> keywordList = new ArrayList<>();
+            for (ExtractResponseItem result : results) {
+                Set<String> keywords = new HashSet<>();
+                for(Extraction extraction : result.extractions) {
+                    keywords.add(extraction.parsedValue);
+                }
+                keywordList.add(keywords);
+            }
+            return keywordList;
+        };
+
+        try {
+            return httpClient.execute(request, responseHandler);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Collections.emptyList();
+    }
+
+    public static void main(String[] args) {
+        List<String> articles = Arrays.asList(
+                "In this project, we will use SpaceX launch data such as rocket launch timing and star chain satellite position for visualization and front-end development.You will be introduced to React JS, relevant front-end technologies, and data interaction functions",
+                "Studying this project is helpful for interviews with companies such as Google, LinkedIn, Facebook, Amazon, Yelp, TripAdvisor and Airbnb.",
+                "Social media video platforms such as TikTok and Kuaishou have become more and more popular."
+        );
+
+        MonkeyLearnClient client = new MonkeyLearnClient();
+
+        List<Set<String>> keywordList = client.extract(articles);
+        System.out.println(keywordList);
+    }
+}
